5f976340af588d4165a7e8352b425f21d8e78cb3
Sander van der Burg <svanderburg@gmail.com>
2020-06-22 23:52:34 +0200

    Add module that derives the appropriate Dysnomia plugin for a certain process manager

---
+++ nixproc/derive-dysnomia-process-type.nix
@@ -0,0 +1,11 @@
+{processManager}:
+
+if processManager == null then "managed-process"
+else if processManager == "sysvinit" then "sysvinit-script"
+else if processManager == "systemd" then "systemd-unit"
+else if processManager == "supervisord" then "supervisord-program"
+else if processManager == "bsdrc" then "bsdrc-script"
+else if processManager == "cygrunsrv" then "cygrunsrv-service"
+else if processManager == "launchd" then "launchd-daemon"
+else if processManager == "disnix" then "process"
+else throw "Unknown process manager: ${processManager}"
