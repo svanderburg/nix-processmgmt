53dee99baf2ca283c5ab7c2374001e877b38308a
Sander van der Burg <svanderburg@gmail.com>
2021-03-19 21:27:10 +0100

    Create nixproc-disnix-deploy, because disnix-deploy can't initialize global state

--- tools/disnix/default.nix
+++ tools/disnix/default.nix
@@ -8,6 +8,12 @@ stdenv.mkDerivation {
     sed -e "s|/bin/bash|$SHELL|" \
       -e "s|@getopt@|${getopt}/bin/getopt|" \
       -e "s|@commonchecks@|${../commonchecks}|" \
+      ${./nixproc-disnix-deploy.in} > $out/bin/nixproc-disnix-deploy
+    chmod +x $out/bin/nixproc-disnix-deploy
+
+    sed -e "s|/bin/bash|$SHELL|" \
+      -e "s|@getopt@|${getopt}/bin/getopt|" \
+      -e "s|@commonchecks@|${../commonchecks}|" \
       ${./nixproc-disnix-switch.in} > $out/bin/nixproc-disnix-switch
     chmod +x $out/bin/nixproc-disnix-switch
   '';
