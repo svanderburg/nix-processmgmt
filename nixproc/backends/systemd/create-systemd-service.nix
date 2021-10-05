a70861dcf236d8ca792050ccf4e11a8eed9399a1
Julio Sueiras <juliosueiras@gmail.com>
2021-10-05 02:25:27 -0400

    Update create-systemd-service.nix

--- nixproc/backends/systemd/create-systemd-service.nix
+++ nixproc/backends/systemd/create-systemd-service.nix
@@ -70,7 +70,11 @@ let
         value = builtins.getAttr name properties;
       in
       if forceDisableUserChange && (name == "User" || name == "Group") then "" else # Don't change user privileges when we force it to be disabled
-      ''${name}=${toString value}
+      ''${if builtins.isList value then
+        lib.strings.concatMapStringsSep "\n" (x: "${name}=${toString x}") value
+      else
+        "${name}=${toString value}"
+      end}
       ''
     ) (builtins.attrNames properties)}''
   + (if title == "Service" then generateEnvironmentVariables _environment else "")
